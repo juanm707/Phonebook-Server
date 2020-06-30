a535c424d9c053d6010fcbded29c8e7d3b1fbafd
juanmartinez <juanm.jm49@gmail.com>
2020-06-30 14:56:49 -0700

    added front-end

---
+++ build/precache-manifest.afca8f6509699a77746d9ed741c78b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28989c6f2687d9e885bccd16e220f86c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeea59d82dbeacb719f8",
+    "url": "/static/css/main.11ca6cd4.chunk.css"
+  },
+  {
+    "revision": "6950fc1a529b1967a812",
+    "url": "/static/js/2.62fb2af6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62fb2af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeea59d82dbeacb719f8",
+    "url": "/static/js/main.a1bbd1ab.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
